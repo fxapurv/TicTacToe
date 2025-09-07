633ebe5b80ae40a581163387c7997f0c42fc79c8
Apurv Anand <adirajabc123@gmail.com>
2025-09-07 11:14:23 +0530

    winner logic

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let winningPatterns = [
     [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
-    [1, 4 ,7],
+    [1, 4, 7],
     [2, 4, 6],
     [2, 5, 8],
     [3, 4, 5],
@@ -19,14 +19,28 @@ boxes.forEach((box) => {
     box.addEventListener("click", () => {
         console.log("button was clicked");
 
-        if(turnO){
+        if (turnO) {
             box.innerText = "O";
             turnO = false;
-        }else{
+        } else {
             box.innerText = "X";
             turnO = true;
         }
         box.disabled = true;
+        checkWinner();
     });
 });
 
+const checkWinner = () => {
+    for (let pattern of winningPatterns) {
+        let val1 = boxes[pattern[0]].innerText;
+        let val2 = boxes[pattern[1]].innerText;
+        let val3 = boxes[pattern[2]].innerText;
+
+        if (val1 != "" && val2 != "" && val3 != "") {
+            if (val1 === val2 && val2=== val3) {
+                console.log("winner", val1);
+            }
+        }
+    }
+}
